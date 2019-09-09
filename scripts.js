376b8f9650a21853e6b0eaed67da32386632955d
MehalaKeller <55070236+MehalaKeller@users.noreply.github.com>
2019-09-08 20:55:30 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,42 @@
+
+function myFunction() {
+	if (document.getElementById("changeImage").src == "https://i.kym-cdn.com/photos/images/newsfeed/000/229/161/fd5.jpg")
+	{
+	 document.getElementById("changeImage").src = "https://static.fjcdn.com/pictures/Childhood+ruined_db52c6_4049312.jpg"
+	 document.getElementById("title").innerHTML = "I changed";
+	}
+	else if(document.getElementById("changeImage").src == "https://static.fjcdn.com/pictures/Childhood+ruined_db52c6_4049312.jpg")
+	{
+	 document.getElementById("changeImage").src = "https://sayingimages.com/wp-content/uploads/beast-disney-princess-memes.jpg";
+	 document.getElementById("title").innerHTML = "I changed";
+	}
+	else if (document.getElementById("changeImage").src == "https://sayingimages.com/wp-content/uploads/beast-disney-princess-memes.jpg")
+	{
+	 document.getElementById("changeImage").src = "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/terminal01/2011/2/22/15/enhanced-buzz-30302-1298404880-11.jpg";
+	 document.getElementById("title").innerHTML = "I changed";
+	}
+	else if (document.getElementById("changeImage").src == "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/terminal01/2011/2/22/15/enhanced-buzz-30302-1298404880-11.jpg")
+	{
+	 document.getElementById("changeImage").src = "https://i.kym-cdn.com/photos/images/newsfeed/000/229/161/fd5.jpg";
+	 document.getElementById("title").innerHTML = "I changed";
+	}
+}
+
+function Cinderella() {
+	document.getElementById("quote").innerHTML = "Cinderella lied and snuck out at night to attend a party.";
+}
+function Aurora() {
+	document.getElementById("quote").innerHTML = "Aurora married a creep who kissed her in her sleep.";
+}
+function Belle() {
+	document.getElementById("quote").innerHTML = "Belle committed beasteality.";
+}
+function Jasmine() {
+	document.getElementById("quote").innerHTML = "Jasmine married a dirty homeless thief.";
+}
+function Ariel() {
+	document.getElementById("quote").innerHTML = "Ariel risked her soul for a man she never met.";
+}
+function SnowWhite() {
+	document.getElementById("quote").innerHTML = "Snow White lived alone with 7 men.";
+}
